79bcb617d16749cf6bc7da2076f46a64287baa51
Faith <milkgirl96@yahoo.com>
2019-06-23 23:20:25 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // Don't call this.setState() here!
+    this.state = {
+      pokemon: []
+    }
+    this.getData = this.getData.bind(this);
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    Axios.get("https://intern-pokedex.myriadapps.com/api/v1/pokemon?page=2")
+    .then(res => {
+      console.log(res);
+      this.setState({
+        pokemon: res.data.data
+      })
+    })
+  }
+
+  render() {
+    console.log(this.state.pokemon);
+    let pokemon = this.state.pokemon.map((pokemon) => {
+      return (
+        <>
+          <h1>{pokemon.name}</h1>
+          <img src={pokemon.image} />
+        </>
   );
+});
+    return (
+      
+        <div class="App">
+        {pokemon}
+        </div>
+    );
+  }
 }
 
 export default App;
